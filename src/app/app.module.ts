7391f903a232cc74d5f0d4c55888b9dd84314d78
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2021-06-29 22:15:02 -0600

    Plantilla inicial Versión 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LeaguesComponent } from './components/leagues/leagues.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    LeaguesComponent,
+    TeamsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
